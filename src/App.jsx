3d459389532c9b77e9b53a2e8d8f792140dae58b
victors21dev <santos.victors2000@gmail.com>
2025-12-12 23:08:02 -0300

    chore: Add setup project vite+react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container w-full h-full m-auto">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>aaaaaaaaaa</p>
+    </div>
+  );
+}
+
+export default App;
