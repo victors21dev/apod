39dea1673a2b033552ff0fb7005dc9da1ce392e0
victors21dev <santos.victors2000@gmail.com>
2025-12-16 21:34:46 -0300

    refactor: Modifid handleDownload

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -28,6 +28,25 @@ function CardDetail({ data }) {
     setIsLoadingHD(false);
   };
 
+  const handleDownload = () => {
+    const downloadUrl = data.hdurl;
+
+    if (downloadUrl) {
+      const filename = `${data.title.replace(/\s/g, "_")}_${data.date}_HD.jpg`;
+
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = filename;
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+
+      console.log(`Download iniciado: ${filename}`);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div className="font-bold text-3xl">{data.title}</div>
@@ -42,13 +61,29 @@ function CardDetail({ data }) {
         )}
 
         {isLoadingHD && (
-          <div className="absolute inset-0 flex items-center justify-center bg-[#2B378C] bg-opacity-75 rounded-lg">
-            <p className="text-white text-lg">Loading...</p>
-          </div>
-        )}
-        {!image && !isLoadingHD && (
-          <div className="flex items-center justify-center w-full h-full bg-gray-200 rounded-lg">
-            Loading image...
+          <div className="absolute inset-0 flex flex-col items-center justify-center bg-gray-900 bg-opacity-75 rounded-lg">
+            <p className="text-white text-lg mb-4">Carregando em HD...</p>
+
+            <svg
+              className="animate-spin h-10 w-10 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
           </div>
         )}
       </div>
@@ -68,11 +103,16 @@ function CardDetail({ data }) {
               {nameButtonView}
             </button>
           </div>
-          <a href={data.hdurl} target="blank">
-            <button className="bg-[#5F2B8C] hover:bg-purple-800 text-white font-bold py-2 px-4 rounded">
+          <div>
+            <button
+              onClick={() => {
+                handleDownload();
+              }}
+              className="bg-[#5F2B8C] hover:bg-purple-800 text-white font-bold py-2 px-4 rounded"
+            >
               Download
             </button>
-          </a>
+          </div>
         </div>
       </div>
     </div>
